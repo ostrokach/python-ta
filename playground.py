419235f83691dd103eab3b8d83b5e0e051a54c41
ostrokach <ostrokach@gmail.com>
2017-07-05 11:19:19 -0400

    Add repo for testing lint errors

---
+++ playground.py
@@ -0,0 +1,7 @@
+def find_factors(number):
+    cross_list = []
+    for x in x_list:
+        for y in y_list:
+            for z in z_list:
+                if x != y != z:
+                    cross_list.append(x, y, z)
