4f5a9d82d24306291765996dbe313694d680fc21
ostrokach <ostrokach@gmail.com>
2017-07-16 12:39:54 -0400

    Update the build website script to watch more files

--- build_website.sh
+++ build_website.sh
@@ -4,14 +4,13 @@ set -v
 
 cd website
 
-while inotifywait -e create -e modify index.md ; do 
-    echo "regenerating..." ; 
+while inotifywait -e create -e modify index.md pyta_template.html styles/main.css ; do 
+    echo "regenerating..." ;
     rc=-1
     while [[ $rc != 0 ]] ; do
-        python build.py	
+        python build.py
 	rc=$?
     done
 done
 
 cd ..
-
