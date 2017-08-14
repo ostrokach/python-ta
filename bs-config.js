9101f08f819e0534cc56dc9ba5db038f83727511
ostrokach <ostrokach@gmail.com>
2017-08-14 12:00:54 -0400

    Don't open browser automatically

--- bs-config.js
+++ bs-config.js
@@ -50,7 +50,7 @@ module.exports = {
     "logFileChanges": true,
     "logSnippet": true,
     "rewriteRules": [],
-    "open": "local",
+    "open": false,
     "browser": "default",
     "cors": false,
     "xip": false,
